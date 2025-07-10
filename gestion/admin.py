fb5f2ca6a32177c5b11f36bf530fdca13d423b63
Ruben-Andrade-Huerta <rubsan0503@gmail.com>
2025-07-09 18:42:40 -0600

    Modelos agregados a admin, y mejora su visualizacion con __str__

--- gestion/admin.py
+++ gestion/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Alumno, Docente, Grupo, GrupoMateria, Materia
 
 # Register your models here.
+admin.site.register(Alumno)
+admin.site.register(Docente)
+admin.site.register(Grupo)
+admin.site.register(GrupoMateria)
+admin.site.register(Materia)
