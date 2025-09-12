a4fa0466990182fba31aa435a6288ef0459717f1
Ruben-Andrade-Huerta <rubsan0503@gmail.com>
2025-09-11 19:08:25 -0600

    Commit para clonar repositorio en pc

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './auth/authProvider';
 import PrivateRoute from './routes/PrivateRoute';
-import Login from './pages/Login';
+import {Login} from './pages/Login'
 import Dashboard from './pages/Dashboard';
 
 function App() {
@@ -10,10 +10,9 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Navigate to='/login'/>}/>
           <Route path="/login" element={<Login />} />
-          <Route
-            path="/dashboard"
-            element={
+          <Route path="/dashboard" element={
               <PrivateRoute>
                 <Dashboard />
               </PrivateRoute>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,2 +1,25 @@
-const Login = () => <div>Login pendiente</div>;
-export default Login;
+import {useForm} from 'react-hook-form'
+
+const {register, handleSubmit} = useForm()
+
+const onSubmitform = handleSubmit(data => console.log(data))
+
+export function Login() {
+  return (
+    <div>
+      <h1>Control Escolar CCV</h1>
+      <form action="" onSubmit={onSubmitform}>
+        <div>
+          <input id="email" placeholder="Correo" type="email" required {...register("email")}/>
+        </div>
+        <div>
+          <input type="password" placeholder="Contraseña" required {...register("password")}/>
+        </div>
+        <div>
+          <button>Login</button>
+        </div>
+        <a href="">Olvidaste tu contraseña?</a>
+      </form>
+    </div>
+  );
+}
